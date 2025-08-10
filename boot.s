c5d7baff70edfad279a8dc00dbcada4d80cc1275
quyetnguyentan@gmail.com <quyetnguyentan@gmail.com>
2025-08-10 15:10:26 -0600

    Updated boot.s

--- boot.s
+++ boot.s
@@ -1,10 +1,15 @@
 /* boot.s - multiboot header + entry */
+// .section .multiboot
 .section .multiboot
-.align 4
-/* multiboot magic, flags, checksum */
-.long 0x1BADB002
-.long 0x00010003
-.long -(0x1BADB002 + 0x00010003)
+    .byte 0x1b, 0xad, 0xb0, 0x02   # magic number
+    .byte 0x00, 0x01, 0x00, 0x03   # flags
+    .byte 0xe4, 0x52, 0x4f, 0xfb   # checksum (two's complement)
+
+// .align 4
+// /* multiboot magic, flags, checksum */
+// .long 0x1BADB002
+// .long 0x00010003
+// .long -(0x1BADB002 + 0x00010003)
 
 .section .text
 .global start
